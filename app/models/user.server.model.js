4088f1dc3e66b8a1afe05b2548b3a98be290bf95
buirai <buirai_naetle@hotmail.com>
2016-09-23 16:53:47 -0500

    User schema modified

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -4,9 +4,52 @@ var mongoose = require('mongoose'),
 var UserSchema = new Schema({
 	firstName: String,
 	lastName: String,
-	email: String,
-	username: String,
-	password: String
+	email: {
+		type: String,
+		index: true
+	},
+	website: {
+		type: String,
+		get: function(url) {
+			if (!url) {
+				return url;
+			} else {
+				if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+					url = 'http://' + url;
+				}
+				return url;
+			}
+		},
+		set: function(url) {
+			if (!url) {
+				return url;
+			} else {
+				if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+					url = 'http://' + url;
+				}
+				return url;
+			}
+		}
+	},
+	username: {
+		type: String,
+		trim: true,
+		unique: true
+	},
+	password: String,
+	created: {
+		type: Date,
+		default: Date.now
+	}
 });
 
+UserSchema.virtual('fullName').get(function() {
+	return this.firstName + ' ' + this.lastName;
+}).set(function(fullName) {
+	var splitName = fullName.split(' ');
+	this.firstName = splitName[0] || '';
+	this.lastName = splitName[1] || '';
+});
+
+UserSchema.set('toJSON', { getters: true, virtuals: true });
 mongoose.model('User', UserSchema);
